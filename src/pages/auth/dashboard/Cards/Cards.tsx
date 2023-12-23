2bad63763cac49ad37532b11cb1b86f4696cd756
Damian Kliber <damiankliber@gmail.com>
2023-12-23 16:13:12 +0100

    improvement dashboard look

--- src/pages/auth/dashboard/Cards/Cards.tsx
+++ src/pages/auth/dashboard/Cards/Cards.tsx
@@ -1,51 +1,66 @@
 import React from 'react'
 import {
-  Card, CardContent, Typography, Stack, Button, CardMedia,
+  Card, CardContent, Typography, Button, CardMedia, Grid,
 } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import Styles from './Cards.module.scss'
 import { Pages } from '../../../pages'
 import { useAuth } from '../../../../context/auth'
 import { Roles } from '../../../../model'
+import CreateTravel from '../../../../assets/theme/dashboard/create-travel.jpg'
+import TravelPlans from '../../../../assets/theme/dashboard/travel-plans.jpg'
+import RealizedTravels from '../../../../assets/theme/dashboard/realized-travels.jpg'
+import Manage from '../../../../assets/theme/dashboard/manage.jpg'
 
 type CardProps = {
+  image: string
   title: string,
   description: string,
   link: () => any,
 }
 
 const CardComponent: React.FC<CardProps> = (props) => (
-  <Stack>
+  <Grid
+    item
+    key={props.title}
+    xs={12}
+    md={4}
+    lg={6}
+    xl={4}
+  >
     <Card>
       <CardMedia
         sx={{ height: 140 }}
-        image="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
-        title="some title"
+        image={props.image}
       />
       <CardContent>
-        <Typography>
-          <h4
+        <Typography
+          style={{
+            margin: 0,
+          }}
+        >
+          <h3
             className={Styles.cardHeader}
           >
             {props.title}
-          </h4>
+          </h3>
           <div
             className={Styles.cardText}
           >
             {props.description}
           </div>
-
-          <Button
-            className={Styles.cardButton}
-            variant="contained"
-            onClick={props.link}
-          >
-            Przejdź
-          </Button>
         </Typography>
+
+        <Button
+          className={Styles.cardButton}
+          variant="contained"
+          onClick={props.link}
+        >
+          Przejdź
+        </Button>
       </CardContent>
     </Card>
-  </Stack>
+  </Grid>
 )
 
 const Cards: React.FC = () => {
@@ -53,20 +68,24 @@ const Cards: React.FC = () => {
   const { roles } = useAuth()
 
   return (
-    <Stack
-      gap={1}
+    <Grid
+      container
+      spacing={2}
     >
       <CardComponent
+        image={CreateTravel}
         title="Kreator wycieczki"
         description="Użyj narzędzi oferowanych przez naszą witrynę aby stworzyć wycieczkę swoich marzeń"
         link={() => navigate(Pages.CREATE_TRAVEL.getRedirectLink())}
       />
       <CardComponent
+        image={TravelPlans}
         title="Plany wycieczek"
         description="Przeglądaj zaplanowane przez siebie wycieczki"
         link={() => navigate(Pages.TRAVEL_RECIPES_STORE.getRedirectLink())}
       />
       <CardComponent
+        image={RealizedTravels}
         title="Zrealizowane wycieczki"
         description="Przeglądaj odbyte przez siebie wycieczki"
         link={() => navigate(Pages.REALIZED_TRIPS.getRedirectLink())}
@@ -76,6 +95,7 @@ const Cards: React.FC = () => {
         roles?.includes(Roles.ADMIN) && (
           <>
             <CardComponent
+              image={Manage}
               title="Zarządzaj aktywnościami"
               description="Rozwijaj listę aktywności polecanych przez system"
               link={() => navigate(Pages.ADD_ACTIVITY.getRedirectLink(), {
@@ -86,6 +106,7 @@ const Cards: React.FC = () => {
               })}
             />
             <CardComponent
+              image={Manage}
               title="Zarządzaj noclegami"
               description="Rozwijaj listę noclegów polecanych przez system"
               link={() => navigate(Pages.ADD_ACCOMMODATION.getRedirectLink(), {
@@ -98,7 +119,7 @@ const Cards: React.FC = () => {
           </>
         )
       }
-    </Stack>
+    </Grid>
   )
 }
 
